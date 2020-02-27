af25060b1f3104c22fef845ea666d79dd98221f3
harikatakam <urstrulyhariprasad@gmail.com>
2020-02-27 11:06:10 +0530

    refactoring Moving files to nested folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { AppRootComponent } from './component/app-root/app-root.component';
 import { LoginComponent } from './component/login/login.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { UserCreationComponent } from './user-creation/user-creation.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { UserCreationComponent } from './component/user-creation/user-creation.component';
 import { AuthGuard } from './service/auth.guard';
 
 const routes: Routes = [
-  { path: '', component: AppRootComponent, canActivate: [AuthGuard] },
+  { path: '', component: AppRootComponent },  // , canActivate: [AuthGuard]
   { path: 'login', component: LoginComponent },
   { path: 'Dashboard', component: DashboardComponent },
   { path: 'User', component: UserCreationComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,19 +16,19 @@ import { LayoutModule } from '@angular/cdk/layout';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './component/home/home.component';
 import { AppRootComponent } from './component/app-root/app-root.component';
 import { LoginComponent } from './component/login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http'; 
-import { SideMenuBarComponent } from './side-menu-bar/side-menu-bar.component';
-import { HeaderComponent } from './header/header.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { UserCreationComponent } from './user-creation/user-creation.component';
+import { HttpClientModule } from '@angular/common/http';
+import { SideMenuBarComponent } from './component/side-menu-bar/side-menu-bar.component';
+import { HeaderComponent } from './component/header/header.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { UserCreationComponent } from './component/user-creation/user-creation.component';
 import { MdePopoverModule } from '@material-extended/mde';
 import { MatCardModule } from '@angular/material/card';
 @NgModule({
-  declarations: [AppComponent, HomeComponent, AppRootComponent, LoginComponent,SideMenuBarComponent, 
+  declarations: [AppComponent, HomeComponent, AppRootComponent, LoginComponent,SideMenuBarComponent,
     HeaderComponent, DashboardComponent, UserCreationComponent],
   imports: [
     BrowserAnimationsModule,
